aecf02838fbf41bf6ee453bf12092fcbc8cb13d3
abhkshatriya <abhishekshekhar896@gmail.com>
2025-07-28 12:17:37 +0530

    line added by local

--- sample.py
+++ sample.py
@@ -7,8 +7,6 @@ print("change from developer 4")
 print("Change from developer 8")
 print("change from developer 9")
 print("Ineuron")
-<<<<<<< HEAD
 print("Line 10 by Dev1")
-=======
 print("Line 10 by Dev 2")
->>>>>>> 00470acb8b7ec70f87601e987b6ea90aebad7e84
+print("Line added in local")
